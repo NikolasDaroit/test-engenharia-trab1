317f98223d0a2814e647bed074d43fab9fe906a5
Níkolas Daroit <nikolastnd@gmaill.com>
2018-10-05 11:17:15 -0300

    regra de acordo com modelo de sequencia teste para visita

--- app/src/main/java/com/teste/software/trabalho1/controller/VisitaController.java
+++ app/src/main/java/com/teste/software/trabalho1/controller/VisitaController.java
@@ -1,9 +1,43 @@
 package com.teste.software.trabalho1.controller;
 
+import com.teste.software.trabalho1.model.Usuario;
 import com.teste.software.trabalho1.model.Visita;
 
 public class VisitaController {
-	public void salvar() {
+	public Visita salvarDados(
+			String codigo, 
+			Usuario usuario, 
+			String email_usuario, 
+			String data_visita, 
+			String hora_visita, 
+			String nome_instituicao, 
+			String tipo_instituicao, 
+			String cidade_instituicao, 
+			String telefone_instituicao, 
+			Integer numero_visitantes, 
+			Boolean is_visita_guiada, 
+			Boolean has_conteudo_disponivel, 
+			Boolean is_auxilio_conteudo_requisitado, 
+			String conteudo) {
+		
+		if (
+				codigo.length()>0 && 
+				usuario.getNome().length()>0 && 
+				email_usuario.length()>0 && 
+				data_visita.length()>0 && 
+				hora_visita.length()>0 && 
+				nome_instituicao.length()>0 && 
+				tipo_instituicao.length()>0 && 
+				cidade_instituicao.length()>0 && 
+				telefone_instituicao.toString().length()>0 && 
+				numero_visitantes.toString().length()>0 && 
+				conteudo.length()>0 
+				) {
+			
+			Visita visita = new Visita(codigo, usuario, email_usuario, data_visita, hora_visita, nome_instituicao, tipo_instituicao, cidade_instituicao, telefone_instituicao, numero_visitantes, is_visita_guiada, has_conteudo_disponivel, is_auxilio_conteudo_requisitado, conteudo);
+			return visita;
+		}
+		throw new IllegalArgumentException("faltou algo"); 
 		
 	}
 	public Boolean enviaConfirmacao(Visita visita) {
--- app/src/main/java/com/teste/software/trabalho1/model/Visita.java
+++ app/src/main/java/com/teste/software/trabalho1/model/Visita.java
@@ -4,10 +4,35 @@ import lombok.Data;
 
 @Data
 public class Visita {
-	public Visita(String codigo2, Usuario usuario2, String email_usuario2, String data_visita2, String hora_visita2,
-			String nome_instituicao2, String tipo_instituicao2, String cidade_instituicao2,
-			Integer telefone_instituicao2, Integer numero_visitantes2, Boolean is_visita_guiada2,
-			Boolean has_conteudo_disponivel2, Boolean is_auxilio_conteudo_requisitado2, String conteudo2) {
+	private String codigo;
+	private Usuario usuario;
+	private String email_usuario;
+	private String data_visita;
+	private String hora_visita;
+	private String nome_instituicao;
+	private String tipo_instituicao;
+	private String cidade_instituicao;
+	private String telefone_instituicao;
+	private Integer numero_visitantes;
+	private Boolean is_visita_guiada;
+	private Boolean has_conteudo_disponivel;
+	private Boolean is_auxilio_conteudo_requisitado;
+	private String conteudo;
+	
+	public Visita(String codigo,
+			Usuario usuario,
+			String email_usuario,
+			String data_visita,
+			String hora_visita,
+			String nome_instituicao,
+			String tipo_instituicao,
+			String cidade_instituicao,
+			String telefone_instituicao,
+			Integer numero_visitantes,
+			Boolean is_visita_guiada,
+			Boolean has_conteudo_disponivel,
+			Boolean is_auxilio_conteudo_requisitado,
+			String conteudo) {
 		this.codigo=codigo;
 		this.usuario=usuario;
 		this.email_usuario=email_usuario;
@@ -23,18 +48,4 @@ public class Visita {
 		this.is_auxilio_conteudo_requisitado=is_auxilio_conteudo_requisitado;
 		this.conteudo=conteudo;
 	}
-	private String codigo;
-	private Usuario usuario;
-	private String email_usuario;
-	private String data_visita;
-	private String hora_visita;
-	private String nome_instituicao;
-	private String tipo_instituicao;
-	private String cidade_instituicao;
-	private String telefone_instituicao;
-	private Integer numero_visitantes;
-	private Boolean is_visita_guiada;
-	private Boolean has_conteudo_disponivel;
-	private Boolean is_auxilio_conteudo_requisitado;
-	private String conteudo;
 }
